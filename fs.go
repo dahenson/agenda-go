f90ae6732642cd158233078a232261deb6b56613
Dane Henson <thegreatdane@gmail.com>
2013-12-15 15:51:37 -0600

    Formatting changes

--- fs.go
+++ fs.go
@@ -2,8 +2,8 @@ package main
 
 import (
 	"fmt"
-	"os"
 	"io/ioutil"
+	"os"
 	"strings"
 )
 
@@ -15,12 +15,13 @@ type fileSystem interface {
 	IsNotExist(err error) bool
 }
 
+type osFS struct{}
 
-type osFS struct {}
 func (osFS) ReadFile(name string) ([]byte, error) {
 	data, err := ioutil.ReadFile(name)
 	return data, err
 }
+
 func (osFS) Append(name, text string) (int, error) {
 	file, err := os.OpenFile(name, os.O_CREATE|os.O_APPEND|os.O_RDWR, 0660)
 	if err != nil {
@@ -30,24 +31,28 @@ func (osFS) Append(name, text string) (int, error) {
 
 	return file.WriteString(text)
 }
+
 func (osFS) IsNotExist(err error) bool {
 	return os.IsNotExist(err)
 }
-
 
 const (
 	NOT_EXIST_PREFIX = "open "
 	NOT_EXIST_SUFFIX = ": no such file or directory"
 )
+
 type fakeFS struct {
 	files map[string][]byte
 }
+
 func newFakeFS() *fakeFS {
 	return &fakeFS{files: make(map[string][]byte)}
 }
+
 func doesntExistErr(filename string) error {
 	return fmt.Errorf(NOT_EXIST_PREFIX+"%s"+NOT_EXIST_SUFFIX, filename)
 }
+
 func (fs *fakeFS) ReadFile(name string) ([]byte, error) {
 	data, found := fs.files[name]
 	if !found {
@@ -55,6 +60,7 @@ func (fs *fakeFS) ReadFile(name string) ([]byte, error) {
 	}
 	return data, nil
 }
+
 func (fs *fakeFS) Append(name, text string) (int, error) {
 	if _, found := fs.files[name]; found {
 		fs.files[name] = append(fs.files[name], []byte(text)...)
@@ -63,6 +69,7 @@ func (fs *fakeFS) Append(name, text string) (int, error) {
 	}
 	return len(text), nil
 }
+
 func (fs *fakeFS) IsNotExist(err error) bool {
 	if err == nil {
 		return false
